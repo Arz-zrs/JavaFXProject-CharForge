130a7b0975897b30093d381a1e957e45f0a13895
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2025-12-11 22:27:21 +0800

    Implemented paper doll UI and inventory system, refactored and adjusted backend to fit inventory system

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,9 @@ module com.project.charforge {
     requires java.sql;
     requires java.management;
 
-    // Controller must be exported AND opened
     opens com.project.charforge.controller to javafx.fxml;
     exports com.project.charforge.controller;
 
-    // App entry point
     exports com.project.charforge.app;
     opens com.project.charforge.app to javafx.graphics;
 }
